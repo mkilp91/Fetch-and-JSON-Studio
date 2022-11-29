4bdd38154fbf75a63d6e9364743048c5b51c0173
mkilp91 <mitchellakilpatrick@gmail.com>
2022-11-28 21:19:28 -0600

    added a count

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // TODO: add code here
 window.addEventListener('load', function() {
     const container = document.getElementById('container');
+    const count = document.getElementById('count');
     // let sortedAstronauts = [];
     // console.log(sortedAstronauts);
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(data){
             console.log(data);
+
+            count.innerHTML+=`There are ${data.length} astronauts!`;
 
             data.sort(function(a, b){
             return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
@@ -13,6 +16,7 @@ window.addEventListener('load', function() {
                 for (let i=0; i<data.length; i++) {
                     container.innerHTML+=(`
                     <div class="astronaut">
+                   
                     <div class="bio">
                     <h3>${data[i].firstName} ${data[i].lastName}</h3>
                     <ul>
